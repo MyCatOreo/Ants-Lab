253cd8bd82473f205e27b53af531901484ee542b
Lily Yang <neixingren@gmail.com>
2020-03-29 22:38:56 +1100

    navbar style

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { ModulePageComponent } from './components/module-page/module-page.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { ModulePageComponent } from "./components/module-page/module-page.component";
+import { NavbarModule, MenuModule } from "src/libs/_index";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ModulePageComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, ModulePageComponent],
+  imports: [BrowserModule, AppRoutingModule, NavbarModule, MenuModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
