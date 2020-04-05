acb073ed2df21abdcc69072a6e7f0317c4a61e93
Lily Yang <neixingren@gmail.com>
2020-04-05 17:32:44 +1000

    add wiki list active route style

--- src/model/task.ts
+++ src/model/task.ts
@@ -3,3 +3,26 @@ export interface Task {
   name: string;
   description: string;
 }
+
+export const mockupTask = [
+  {
+    id: 1,
+    name: "Nurture",
+    description: "to do",
+  },
+  {
+    id: 2,
+    name: "Construction",
+    description: "to do",
+  },
+  {
+    id: 3,
+    name: "Forage",
+    description: "to do",
+  },
+  {
+    id: 4,
+    name: "Patrol",
+    description: "to do",
+  },
+];
